5da93208fae0bc75e71a4b4798b70c4fd5629b6f
lu9932323258@gmail.com <103122284+Luciana-Pessoa@users.noreply.github.com>
2024-09-13 10:40:16 -0300

    criando funcao

---
+++ js/app.js
@@ -0,0 +1,6 @@
+//cria a funcao do botao
+function alterarStatus(id) {
+//cria a variavel do botao com o id (o sinal de dolar concatena o id nao pelo nome e sim pela variavel)
+let gameClicado = document.getElementById(`game-1${id} `);
+
+}
