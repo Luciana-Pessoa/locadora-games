eab1196e5ee7fde474b5736dfa91271a491d81ad
lu9932323258@gmail.com <103122284+Luciana-Pessoa@users.noreply.github.com>
2024-09-13 11:04:50 -0300

    a

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
+
 //cria a funcao do botao
 function alterarStatus(id) {
-//cria a variavel do botao com o id (o sinal de dolar concatena o id nao pelo nome e sim pela variavel)
+
+    //cria a variavel do botao com o id (o sinal de dolar concatena o id nao pelo nome e sim pela variavel)
 let gameClicado = document.getElementById(`game-1${id} `);
+
+//variavel para pegar a div e o que for clicado (a variavel diz, game clicado pegue a classe com a variavel dashboard__item__img e pegue o elemento da pagina)
+let = gameClicado.querySelector('.dashboard__item__img');
+let = botao.querySelector('.dashboard__item__img');
 
 }
